81585b1705e9de4a8c177d3a6a732ac114e24034
Sandip <sandipdangi682@gmail.com>
2024-04-20 21:43:12 -0700

    d

---
+++ mypractice/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./Greet";
+import Welcome from "./Welcome";
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ mypractice/src/Greet.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Greet() {
+  return <h1>Hello,sandip . Nice to Meet You.</h1>;
+}
+export default Greet;
